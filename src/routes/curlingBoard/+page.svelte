0966e4c792acf748c2d5dd2915a153268f1cb5c1
Jean-Christophe Rondy-Turcotte <taipingpope@gmail.com>
2024-03-23 15:11:08 -0400

    drawing the sheet

--- src/routes/curlingBoard/+page.svelte
+++ src/routes/curlingBoard/+page.svelte
@@ -1,12 +1,34 @@
 <!-- TODO have constants defined for arena height and width -->
 <script>
+	const c_height = 300;
+	const c_width = 600;
+	const y_circle_center = c_height / 2;
+	const x_circle_center = (c_width * 12) / 27;
+
 	import { onMount } from 'svelte';
 	let canvas;
 	onMount(() => {
-        const x_circle_center = 
 		const ctx = canvas.getContext('2d');
+		ctx.beginPath();
+		ctx.arc(x_circle_center, y_circle_center, (6 / 33) * c_width, 0, 2 * Math.PI, false);
+		ctx.fillStyle = 'blue';
+		ctx.fill();
+		ctx.beginPath();
+
+		ctx.arc(x_circle_center, y_circle_center, (4 / 33) * c_width, 0, 2 * Math.PI, false);
+		ctx.fillStyle = 'white';
+		ctx.fill();
+		ctx.beginPath();
+
+		ctx.arc(x_circle_center, y_circle_center, (2 / 33) * c_width, 0, 2 * Math.PI, false);
 		ctx.fillStyle = 'red';
-		ctx.fillRect(0, 0, 150, 75);
+		ctx.fill();
+		ctx.beginPath();
+
+		ctx.arc(x_circle_center, y_circle_center, (0.5 / 33) * c_width, 0, 2 * Math.PI, false);
+		ctx.fillStyle = 'yellow';
+		ctx.fill();
+		ctx.beginPath();
 	});
 </script>
 
@@ -14,7 +36,7 @@
 <canvas
 	bind:this={canvas}
 	id="curlingSheet"
-	height="300"
-	width="660"
+	height={c_height}
+	width={c_width}
 	style="border:1px solid black;"
 ></canvas>
