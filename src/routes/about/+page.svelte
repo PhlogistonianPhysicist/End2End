d21f922f6413d15d59d56896fc91e4b5fccf1fef
Jean-Christophe Rondy-Turcotte <taipingpope@gmail.com>
2024-03-23 15:01:46 -0400

    canvas

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,1 @@
-<nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
-</nav>
 <p>E2E is a new curling app! lorem ipsum etc</p>
---
+++ src/routes/curlingBoard/+page.svelte
@@ -0,0 +1,20 @@
+<!-- TODO have constants defined for arena height and width -->
+<script>
+	import { onMount } from 'svelte';
+	let canvas;
+	onMount(() => {
+        const x_circle_center = 
+		const ctx = canvas.getContext('2d');
+		ctx.fillStyle = 'red';
+		ctx.fillRect(0, 0, 150, 75);
+	});
+</script>
+
+<p>This is where the canvas is</p>
+<canvas
+	bind:this={canvas}
+	id="curlingSheet"
+	height="300"
+	width="660"
+	style="border:1px solid black;"
+></canvas>
